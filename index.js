231e204fd67fad10d1b6dbb106de5e7740630d24
Arslan Akmal <arxlan62@gmail.com>
2023-07-23 07:49:55 -0700

    Third Class

---
+++ index.js
@@ -0,0 +1,67 @@
+// var a=12;
+// var a = 100;
+
+// let b=15;
+// b=16;
+
+// const c=100;
+
+
+// console.log(b);
+
+const a="Arslan";
+const b=23;
+const c=b+" "+a;
+console.log(c)
+
+// If age is less than 20 so print you are young and if age is greater than 20 print you are adult
+// const age=35;
+// if(age<20){
+//     console.log("You are young!")
+// }
+// else if(age>20 && age<30){
+//     console.log("You Are Adult!")
+// }
+// else{
+//     console.log("You are Senior Citizen!")
+// }
+
+const marks=60;
+
+if(marks>=90 && marks<100){
+    console.log("You got A grade!")
+}
+else if(marks>=80 && marks<=90){
+    console.log("You got B  grade")
+}
+else if(marks>=70 && marks<=80){
+    console.log("You got C  grade")
+}
+else if(marks>=60 && marks<=70){
+    console.log("You got D  grade")
+}
+else if(marks>=50 && marks<=60){
+    console.log("You got B  grade")
+}
+else{
+    console.log("You are Fail!")
+}
+
+// Hoisting
+// For Loop
+// For Each Loop
+// Array
+// Array index
+// Object
+// Functions
+
+
+
+
+
+
+// Var => It can redecalre and reinitilize as well
+// Let => It can be reinitilize buyt cannot be redecalre
+// Const => It cannot be reinitilize and redeclare as well
+
+
